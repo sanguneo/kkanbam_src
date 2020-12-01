160d1a00df7d5528455ec593f56649c57125f0d5
나상권 <sknah@urbanbase.com>
2020-12-01 17:09:27 +0900

    디플로이 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+COMMIT_MSG=`git log --pretty=format:"%s" -1`
 mv dist/CNAME backup
 mv dist/.git backup
 npm run generate
@@ -7,6 +7,6 @@ mv backup/CNAME dist
 mv backup/.git dist
 cd dist
 git add .
-git commit -m '$1'
+git commit -m "$COMMIT_MSG"
 git push --force
 cd ..
